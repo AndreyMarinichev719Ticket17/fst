52db9f8346a7f401f4b4237462f018e9a781cfc2
MarinichevAndrey719Ticket17 <MarinichevAndrey719Ticket17@mail.ru>
2022-06-29 16:15:46 +0300

    Marinichev719

---
+++ Скрипт.bat
@@ -0,0 +1,7 @@
+git.exe init
+git.exe add .
+git.exe commit -a -m "first commit"
+git.exe branch -M main
+git.exe remote add origin https://github.com/AndreyMarinichev719Ticket17/fst.git
+git.exe push -u origin main
+pause
